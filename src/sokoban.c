2d58ea0d16d15fa4a8077565d3635c2c08f598a0
Guillaume Bogard <guillaume.bogard-coquard@epitech.eu>
2019-12-11 12:05:55 +0100

    function to check input and to print the map are now split

--- src/sokoban.c
+++ src/sokoban.c
@@ -9,37 +9,50 @@
 #include "sokoban.h"
 #include "my.h"
 
+static void get_input(int *close)
+{
+    switch (getch()) {
+        case KEY_UP:
+            clear();
+            break;
+        case KEY_DOWN:
+            clear();
+            break;
+        case KEY_LEFT:
+            clear();
+            break;
+        case KEY_RIGHT:
+            clear();
+            break;
+        case ' ':
+            (*close) = 1;
+            break;
+        default:
+            refresh();
+            break;
+    }
+}
+
+static void display_map(map_stats_t *map_stats, int *close)
+{
+    int i = 0;
+
+    while (i < map_stats->map_lines) {
+        printw(map_stats->map[i]);
+        i += 1;
+    }
+    get_input(close);
+}
+
 void sokoban(map_stats_t *map_stats)
 {
     int close = 0;
-    int i = 0;
 
     initscr();
     curs_set(FALSE);
     keypad(stdscr, TRUE);
-    while (close == 0) {
-        while (i < map_stats->map_lines) {
-            printw(map_stats->map[i]);
-            i += 1;
-        }
-        switch (getch()) {
-            case KEY_UP:
-                break;
-            case KEY_DOWN:
-                break;
-            case KEY_LEFT:
-                break;
-            case KEY_RIGHT:
-                break;
-            case ' ':
-                close = 1;
-                break;
-            default:
-                refresh();
-                break;
-        }
-    }
+    while (close == 0)
+        display_map(map_stats, &close);
     refresh();
-    getch();
     endwin();
 }
