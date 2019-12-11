af1bfb836e21f2a79be3c229483b13254e21f16a
Guillaume Bogard <guillaume.bogard-coquard@epitech.eu>
2019-12-11 15:42:57 +0100

    -h updated

--- src/error_handling/usage.c
+++ src/error_handling/usage.c
@@ -13,8 +13,8 @@ void usage(void)
     write(1, "USAGE\n", 6);
     write(1, "     ./my_sokoban map\n", 22);
     write(1, "DESCRITION\n", 11);
-    write(1, "     map file representing the warehouse map,", 45);
-    write(1, " containing ‘#’ for walls,\n", 31);
-    write(1, "     ‘P’ for the player, ‘X’ for boxes", 45);
-    write(1, " and ‘O’ for storage locations.\n", 36);
+    write(1, "     map file representing the warehouse map,", 47);
+    write(1, " containing '#' for walls,\n", 31);
+    write(1, "         'P' for the player, 'X' for boxes", 50);
+    write(1, " and 'O' for storage locations.\n", 36);
 }
