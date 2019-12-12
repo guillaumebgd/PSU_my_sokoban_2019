8a261cd11f6134dea9c40e15b469e6591488ad7a
Guillaume Bogard <guillaume.bogard-coquard@epitech.eu>
2019-12-12 02:07:44 +0100

    63% branches coverage

--- tests/tests_move_left_complex.c
+++ tests/tests_move_left_complex.c
@@ -43,3 +43,34 @@ Test(move_left, map_left_get_hole_back_box)
     cr_expect_str_eq(map_stats.map[2], "###############\n");
     free_resources(map_stats);
 }
+
+Test(move_left, move_box_to_wall)
+{
+    map_stats_t map_stats;
+
+    init_map_stats(&map_stats);
+    get_map("./tests/tests_files/move_left/map_left_box_move", &map_stats);
+    move_left(&map_stats);
+    move_left(&map_stats);
+    move_left(&map_stats);
+    move_left(&map_stats);
+    move_left(&map_stats);
+    move_left(&map_stats);
+    cr_expect_str_eq(map_stats.map[0], "###############\n");
+    cr_expect_str_eq(map_stats.map[1], "#XP O         #\n");
+    cr_expect_str_eq(map_stats.map[2], "###############\n");
+    free_resources(map_stats);
+}
+
+Test(move_left, double_boxes_to_push)
+{
+    map_stats_t map_stats;
+
+    init_map_stats(&map_stats);
+    get_map("./tests/tests_files/move_left/map_left_dbl_boxes", &map_stats);
+    move_left(&map_stats);
+    cr_expect_str_eq(map_stats.map[0], "###############\n");
+    cr_expect_str_eq(map_stats.map[1], "#  OOXXP      #\n");
+    cr_expect_str_eq(map_stats.map[2], "###############\n");
+    free_resources(map_stats);
+}
--- tests/tests_move_right_extra.c
+++ tests/tests_move_right_extra.c
@@ -44,3 +44,37 @@ Test(move_right, map_right_obs_player_recover)
     cr_expect_str_eq(map_stats.map[2], "###############\n");
     free_resources(map_stats);
 }
+
+Test(move_right, push_box_to_wall)
+{
+    map_stats_t map_stats;
+
+    init_map_stats(&map_stats);
+    get_map("./tests/tests_files/move_right/map_right_p_o", &map_stats);
+    move_right(&map_stats);
+    move_right(&map_stats);
+    move_right(&map_stats);
+    move_right(&map_stats);
+    move_right(&map_stats);
+    move_right(&map_stats);
+    move_right(&map_stats);
+    move_right(&map_stats);
+    move_right(&map_stats);
+    cr_expect_str_eq(map_stats.map[0], "###############\n");
+    cr_expect_str_eq(map_stats.map[1], "#      O    PX#\n");
+    cr_expect_str_eq(map_stats.map[2], "###############\n");
+    free_resources(map_stats);
+}
+
+Test(move_right, double_boxes_right)
+{
+    map_stats_t map_stats;
+
+    init_map_stats(&map_stats);
+    get_map("./tests/tests_files/move_right/map_right_dbl_boxes", &map_stats);
+    move_right(&map_stats);
+    cr_expect_str_eq(map_stats.map[0], "###############\n");
+    cr_expect_str_eq(map_stats.map[1], "#PXX   OO     #\n");
+    cr_expect_str_eq(map_stats.map[2], "###############\n");
+    free_resources(map_stats);
+}
