7ebcd1503d49749c2db894b0904c655957021938
Guillaume Bogard <guillaume.bogard-coquard@epitech.eu>
2019-12-20 10:45:38 +0100

    usage now properly works

--- src/error_handling/usage.c
+++ src/error_handling/usage.c
@@ -9,11 +9,11 @@
 
 void usage(void)
 {
-    write(1, "USAGE\n", 6);
-    write(1, "     ./my_sokoban map\n", 22);
-    write(1, "DESCRITION\n", 11);
-    write(1, "     map file representing the warehouse map, ", 46);
-    write(1, "containing '#' for walls,\n", 32);
-    write(1, "         'P' for the player, 'X' for boxes", 50);
-    write(1, " and 'O' for storage locations.\n", 36);
+    write(1, "USAGE\n"
+            "     ./my_sokoban map\n"
+            "DESCRITION\n"
+            "     map  file representing the warehouse map, "
+            "containing '#' for walls,\n"
+            "          'P' for the player, 'X' for boxes"
+            " and 'O' for storage locations.\n", 187);
 }
